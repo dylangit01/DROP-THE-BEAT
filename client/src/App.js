88d07f47e2951aebd227d5ca998dcd62709eaa41
Dylan <dcren2010@gmail.com>
2021-06-06 14:12:12 -0400

    add reducer & hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+    const {
+        state,
+        dispatch
+    } = useApplicationData();
+      const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>
+  ));
+  return (<div className="App" >
+    <h1> Users </h1>
+
+    <ul> {userList} </ul>
+  </div >
   );
-}
+};
 
 export default App;
