478aad62c59c8d1d56f8f4042678205cd7154d00
Dylan <dcren2010@gmail.com>
2021-06-07 07:19:50 -0400

    add db data

---
+++ server/db/seeds/03_playlists.sql
@@ -0,0 +1,11 @@
+INSERT INTO playlists(name, image_url, rating, user_id)
+
+VALUES
+  ('Christmas Mood', 'TBD', 4.5, 1),
+  ('K-Poppin', 'TBD', 3.5, 1),
+  ('Quarantine', 'TBD', 0, 1),
+  ('2AM Coding Vibes', 'TBD', 2.5, 1),
+  ('Coffee Shop', 'TBD', 3, 1),
+  ('Beyoncé!', 'TBD', 5, 1),
+  ('Where is the Love', 'TBD', 5, 1),
+  ('Hype x 100', 'TBD', 4, 1);
