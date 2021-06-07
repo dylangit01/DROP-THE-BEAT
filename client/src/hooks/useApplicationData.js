f782b87b17ff6ab3a1c312c4f4c5a17c3d4a9647
Dylan <dcren2010@gmail.com>
2021-06-06 22:03:53 -0400

    add commonts to understand the logic

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -3,13 +3,17 @@ import dataReducer, { SET_USERS } from '../reducer/data_reducer';			// SET_USERS
 import axios from 'axios';
 
 const useApplicationData = () => {
+	// Setup the initial state, but using dispatch to send actions to data_reducer to update the state
 	const [state, dispatch] = useReducer(dataReducer, {
 		users: [],
 		loading: true,
 	});
 
+	/**
+	 * In below axios call, when receiving the returned data, using "dispatch" with matched type to send the action to data_reducer.js, which will update the global state
+	 **/
 	useEffect(() => {
-		axios({ method: 'GET', url: '/api/users', })
+		axios({ method: 'GET', url: '/api/users' })
 			.then(({ data }) => {
 				console.log(data);
 				dispatch({ type: SET_USERS, users: data });
@@ -17,7 +21,7 @@ const useApplicationData = () => {
 			.catch((err) => console.log(err));
 	}, []);
 
-	return { state, dispatch, };
+	return { state, dispatch };
 };
 
 export default useApplicationData;
--- server/app.js
+++ server/app.js
@@ -17,6 +17,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
+
+// All queries are from the Helper fn -- dbHelpers(db)
+/** The logic is:
+ * 1. dbHelpers contain the sql queries, so need the db, also the helper fn returns: {getUsers, getUserByEmail, addUser, getUsersPosts,}
+ * 2. All the returned fns from dbHelpers are not directly used by routes-users.js, instead they are as variables wrapped in a closure fn in users.js and as default export
+ * 3. Below app.use fn has two parameters: user endpoint and usersRouter that contains different users routes, which accepts those variables that come from dbHelpers' returned value
+ * 4. Whenever frontend send the http request ('/api/users') to the server, server app.js will check matched route and transfer to usersRouter to find correct route to deal with db
+ * 5. e.g: when receiving http request '/api/users', the usersRouter will find the matched route "with" matched dbHelpers fns to communicate with db and return the value: "result.rows"
+ * 6. And returned result.rows will be returned to frontend by using ".then((users) => res.json(users))" in the users.js
+ **/
+
 app.use('/api/users', usersRouter(dbHelpers));
 
 module.exports = app;
