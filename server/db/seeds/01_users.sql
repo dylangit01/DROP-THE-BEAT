83a71e38f8542db4c5bb12865c4ada80a7709881
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-07 17:55:15 -0400

    Update schema and seeds

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,18 +1,6 @@
-INSERT INTO
-users(first_name, last_name, email, password)
+INSERT INTO users(username, email, password)
+
 VALUES
-    ('Mario', 'Bros', 'mario@nintendo.com', 'test'),
-    ('Luigi', 'Bros', 'luigi@nintendo.com', 'test'),
-    (
-        'Princess',
-        'Peach',
-        'peach@nintendo.com',
-        'test'
-    ),
-    (
-        'Princess',
-        'Daisy',
-        'daisy@nintendo.com',
-        'test'
-    ),
-    ('Donkey', 'Kong', 'donkey@nintendo.com', 'test');
+  ('DJ Dylan', 'dylan@example.com', 'test'),
+  ('NellySharkiston', 'nelly@example.com', 'test'),
+  ('RubyOffTheRails', 'ruby@example.com', 'test');
