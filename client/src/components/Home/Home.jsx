7a82af3b0b5e4b97930a7393fef6c615b48edc44
Nelli Butenko <butenkonelly93@gmail.com>
2021-06-08 17:08:07 +0000

    Fix component name Lobby=>Join

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,16 +4,16 @@ import {
   Link
 } from "react-router-dom";
 import './Home.scss';
-import Lobby from "../Lobby/Lobby"
+import Join from "../Join/Join"
 
 export default function Home() {
   return (
     <div className="home">
-      <h1>I am Home again</h1>
-      <Link to="/lobby"> Do you want to join the existing game? </Link> <br />
+      <h1>I am Home</h1>
+      <Link to="/join"> Do you want to join the existing game? </Link> <br />
       <Link to="/login"> Do you want to host a new game? </Link>
-      <Route path="/lobby" exact>
-        <Lobby />
+      <Route path="/join" exact>
+        <Join />
       </Route>
     </div>
   );
