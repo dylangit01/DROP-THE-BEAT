433f17eb040adec062e1913bdad4345ccf210c5d
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-08 14:26:20 -0400

    Update button styles

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 
 // Styles
 import './Home.scss';
 import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    background: "linear-gradient(45deg, #9e66f2 30%, #21CBF3 90%)",
+    color: "white",
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
+
   return (
     <div className="home">
         <img className="headphones-img" src="https://github.com/dylangit01/DROP-THE-BEAT/blob/ruby/feature/navbar-home/client/public/images/headphones.png?raw=true" alt="logo" />
@@ -19,11 +28,11 @@ export default function Home() {
         <div className="call-to-action">
           <div>
             <p>Want to host a game?</p>
-            <Button variant="outlined" style={{width: '200px'}} color="inherit"><Link to="/playlists">Select a Playlist</Link></Button>
+            <Button variant="contained" style={{width: '200px'}} className={classes.button}><Link to="/playlists">Select a Playlist</Link></Button>
           </div>
           <div>
             <p>Have a game code?</p>
-            <Button variant="outlined" style={{width: '200px'}} color="inherit"><Link to="/join">Join A Game</Link></Button>
+            <Button variant="contained" style={{width: '200px'}} className={classes.button}><Link to="/join">Join A Game</Link></Button>
           </div>
         </div>
 
