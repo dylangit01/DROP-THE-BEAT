4b3af53aa4943b2c2e1e886cdba93061c7e6bd33
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-08 12:23:37 -0400

    Working version of navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -8,16 +8,16 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import ToggleOnIcon from '@material-ui/icons/ToggleOn';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   appBar: {
-    backgroundColor: "black",
-    opacity: "50%" 
+    // Black background with 30% opacity
+    background: "rgba(0, 0, 0, 0.3)",
   },
   musicButton: {
     marginRight: theme.spacing(3),
@@ -32,12 +32,6 @@ export default function Navbar() {
 
   return (
     <div className={classes.root}>
-      {/* <nav>
-        <h1>I am Navbar</h1>
-        <Link to="/"> Home    </Link>
-        <Link to="/playlists"> My playlists    </Link>
-        <Link to="/game"> My game    </Link>
-      </nav> */}
         <AppBar position="static" className={classes.appBar}>
           <Toolbar>
             <div className="mainNavbarGroup">
@@ -49,11 +43,15 @@ export default function Navbar() {
                     <Link to="/">DROP THE BEAT</Link>
                   </Typography>
                 </Button>
+                <Button color="inherit"><Link to="/playlists">Playlists</Link></Button>
+                <Button color="inherit"><Link to="/join">Join A Game</Link></Button>
+
               </div>
 
               {/* Right Navbar Group */}
               <div className="rightNavbarGroup">
-                <Button color="inherit">Login</Button>
+                <Button color="inherit"><Link to="/login">Login</Link></Button>
+                <Button color="inherit"><ToggleOnIcon /></Button>
               </div>
             </div>
           </Toolbar>
