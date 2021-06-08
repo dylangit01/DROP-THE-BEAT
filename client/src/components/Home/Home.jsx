6ced991118a8bc8cd338283bad91361e6e7d6538
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-08 13:06:10 -0400

    Add headphones image

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -10,11 +10,26 @@ export default function Home() {
   return (
     <div className="home">
       <h1>I am Home again</h1>
-      <Link to="/lobby"> Do you want to join the existing game? </Link> <br />
-      <Link to="/login"> Do you want to host a new game? </Link>
-      <Route path="/join" exact>
-        <Join />
-      </Route>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Link to="/lobby"> Do you want to join the existing game? </Link> <br />
+        <Link to="/login"> Do you want to host a new game? </Link>
+        <Route path="/join" exact>
+          <Join />
+        </Route>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
     </div>
   );
 };
