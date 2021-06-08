b39c872ebd4032fb601e96e1dee585b7d375be5c
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-08 18:25:53 -0400

    Update logic for playlistPage so that there's no error if someone goes directly to /playlist/:id (rendered conditionally)

--- client/src/components/PlaylistPage/PlaylistPage.jsx
+++ client/src/components/PlaylistPage/PlaylistPage.jsx
@@ -10,16 +10,18 @@ export default function PlaylistPage({playlists}) {
   // map through the songs 
 
   return (
-
-      <div className="playlist-page">
-        <h1>I am PlaylistPage</h1>
-        <h3>{playlist.playlistName}</h3>
-        <img className="album-cover" src={playlist.playlistPhoto} alt="cover"></img>
-        <p>Difficulty:</p>
-        <p>Songs:</p>
-        <p>Code:</p>
-        <Link to="/game">Start game</Link>
-      </div>
-
+    <> 
+      {playlist && (
+        <div className="playlist-page">
+          <h1>I am PlaylistPage</h1>
+          <h3>{playlist.playlistName}</h3>
+          <img className="album-cover" src={playlist.playlistPhoto} alt="cover"></img>
+          <p>Difficulty:</p>
+          <p>Songs:</p>
+          <p>Code:</p>
+          <Link to="/game">Start game</Link>
+        </div>
+      )}
+    </>
   );
 };
