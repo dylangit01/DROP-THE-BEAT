725870119aafbc5482a2068229f129fc9ea91135
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-08 09:15:27 -0400

    Add PlaylistPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import useApplicationData from './hooks/useApplicationData';
 // Components
 import Home from "./components/Home/Home";
 import Playlists from "./components/Playlists/Playlists";
+import PlaylistPage from "./components/PlaylistPage/PlaylistPage";
 import Join from "./components/Join/Join";
 import Game from "./components/Game/Game";
 
@@ -24,20 +25,25 @@ function App () {
   return (
     <Router>
       <div className="App" >
-
+        {/* Navbar */}
         <nav>
           <Link to="/"> Home </Link>
           <Link to="/playlists"> My playlists </Link>
           <Link to="/game"> My game </Link>
         </nav>
         
+        {/* Navigation Views */}
         <Switch>
           <Route path="/" exact>
             <Home />
           </Route>
 
           <Route path="/playlists" exact>
-            <Playlists/>
+            <Playlists />
+          </Route>
+
+          <Route path="/playlists/:id" exact>
+            <PlaylistPage />
           </Route>
 
           <Route path="/join" exact>
@@ -50,7 +56,6 @@ function App () {
         </Switch>
       </div >
     </Router>
-
   );
 };
 
