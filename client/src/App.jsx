e0e374ee4d982d34c54529f292c14025612fb1ad
Nelli Butenko <butenkonelly93@gmail.com>
2021-06-08 17:06:54 +0000

    Pass down playlists props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,23 +21,21 @@ import Game from "./components/Game/Game";
 function App () {
 
   const { state, dispatch } = useApplicationData();
-  
+  console.log('State' + state)
   const userList = state.users.map((user) => (<li key={user.id} > {user.first_name} {user.last_name} {user.email} </li>));
 
   return (
     <Router>
       <div className="App" >
-        {/* Navbar */}
         <Navbar />
         
-        {/* Navigation Views */}
         <Switch>
           <Route path="/" exact>
             <Home />
           </Route>
 
           <Route path="/playlists" exact>
-            <Playlists />
+            <Playlists playlists={state.playlists}/>
           </Route>
 
           <Route path="/playlists/:id" exact>
