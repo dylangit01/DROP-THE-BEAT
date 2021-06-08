1734911615e18ad741b3300502c47e3bfe9eb74c
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-08 11:06:49 -0400

    Update Lobby component imports

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import {
   Link
 } from "react-router-dom";
 import './Home.scss';
-import Lobby from "../Lobby/Lobby"
+import Join from "../Join/Join"
 
 export default function Home() {
   return (
@@ -12,8 +12,8 @@ export default function Home() {
       <h1>I am Home again</h1>
       <Link to="/lobby"> Do you want to join the existing game? </Link> <br />
       <Link to="/login"> Do you want to host a new game? </Link>
-      <Route path="/lobby" exact>
-        <Lobby />
+      <Route path="/join" exact>
+        <Join />
       </Route>
     </div>
   );
