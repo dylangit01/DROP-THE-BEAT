78e989e3ff15eb7d251b4e909b0a7def9b0a4d1a
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-08 08:40:25 -0400

    Clean up useApplicationData hook

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from 'react';
-import dataReducer, { SET_PLAYLISTS, SET_SONGS } from '../reducer/data_reducer';
+import dataReducer, { SET_PLAYLISTS } from '../reducer/data_reducer';
 import axios from 'axios';
 
 const useApplicationData = () => {
@@ -9,32 +9,10 @@ const useApplicationData = () => {
     loading: true,
   });
   useEffect(() => {
-    // Get all playlists and songs for a user (hard-coded)
-    // Promise.all([axios.get('/api/playlists'), axios.get('/api/songs')])
-    //   .then((all) => {
-    //     const [playlistsResponse, songsResponse] = all;
-    //     const playlistsData = playlistsResponse.data;
-    //     const songsData = songsResponse.data;
-
-    //     // console.log(playlistsData);
-    //     // console.log(songsData);
-
-    //     dispatch({ type: SET_PLAYLISTS, playlists: playlistsData });
-    //     dispatch({ type: SET_SONGS, songs: songsData });
-    //   })
-    //   .catch((error) => {
-    //     console.log('Error: ', error);
-    //   });
-    axios({
-      method: 'GET',
-      url: '/api/songs',
-    })
+    axios({ method: 'GET', url: '/api/songs' })
       .then(({ data }) => {
         console.log(data);
-        dispatch({
-          type: SET_PLAYLISTS,
-          playlists: data,
-        });
+        dispatch({ type: SET_PLAYLISTS, playlists: data });
       })
       .catch((err) => console.log(err));
 
