3d884eb3af3b9c3e48f4ab5a51bbcf9e81c1303a
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-07 22:00:22 -0400

    Make axios call to backend to get data for playlists and songs

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -1,31 +1,53 @@
 import { useEffect, useReducer } from 'react';
-import dataReducer, { SET_USERS } from '../reducer/data_reducer';
+import dataReducer, { SET_PLAYLISTS, SET_SONGS } from '../reducer/data_reducer';
 import axios from 'axios';
 
 const useApplicationData = () => {
-	const [state, dispatch] = useReducer(dataReducer, {
-		users: [],
-		loading: true,
-	});
-	useEffect(() => {
-		axios({
-			method: 'GET',
-			url: '/api/users',
-		})
-			.then(({ data }) => {
-				console.log(data);
-				dispatch({
-					type: SET_USERS,
-					users: data,
-				});
-			})
-			.catch((err) => console.log(err));
-	}, []);
+  const [state, dispatch] = useReducer(dataReducer, {
+    users: [],
+    songs: [],
+    playlists: [],
+    loading: true,
+  });
+  useEffect(() => {
+    // Get all playlists and songs for a user (hard-coded)
+    Promise.all([
+      axios.get('/api/users/1/playlists'),
+      axios.get('/api/users/1/songs'),
+    ])
+      .then((all) => {
+        const [playlistsResponse, songsResponse] = all;
+        const playlistsData = playlistsResponse.data;
+        const songsData = songsResponse.data;
 
-	return {
-		state,
-		dispatch,
-	};
+        // console.log(playlistsData);
+        // console.log(songsData);
+
+        dispatch({ type: SET_PLAYLISTS, playlists: playlistsData });
+        dispatch({ type: SET_SONGS, songs: songsData });
+      })
+      .catch((error) => {
+        console.log('Error: ', error);
+      });
+
+    // axios({
+    //   method: 'GET',
+    //   url: '/api/users',
+    // })
+    //   .then(({ data }) => {
+    //     console.log(data);
+    //     dispatch({
+    //       type: SET_USERS,
+    //       users: data,
+    //     });
+    //   })
+    //   .catch((err) => console.log(err));
+  }, []);
+
+  return {
+    state,
+    dispatch,
+  };
 };
 
 export default useApplicationData;
--- client/src/reducer/data_reducer.js
+++ client/src/reducer/data_reducer.js
@@ -1,16 +1,18 @@
 export const SET_USERS = 'SET_USERS';
+export const SET_PLAYLISTS = 'SET_PLAYLISTS';
+export const SET_SONGS = 'SET_SONGS';
 
 const dataReducer = (state, action) => {
-	switch (action.type) {
-		case SET_USERS:
-			return {
-				...state,
-				users: action.users,
-				loading: false,
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case SET_USERS:
+      return { ...state, users: action.users, loading: false };
+    case SET_PLAYLISTS:
+      return { ...state, playlists: action.playlists };
+    case SET_SONGS:
+      return { ...state, songs: action.songs };
+    default:
+      return state;
+  }
 };
 
 export default dataReducer;
