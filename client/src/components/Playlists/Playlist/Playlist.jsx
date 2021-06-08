49d11cc341f0f5861f8baa2bc330f513710b4956
Nelli Butenko <butenkonelly93@gmail.com>
2021-06-08 17:09:46 +0000

    Pass down props and display all playlists on the page

--- client/src/components/Playlists/Playlist/Playlist.jsx
+++ client/src/components/Playlists/Playlist/Playlist.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './Playlist.scss';
+import PlaylistPage from "../../PlaylistPage/PlaylistPage";
 
-export default function Playlist() {
+export default function Playlist({name, image, rating}) {
   return (
     <div className="playlist">
-      <h1>I am Playlist</h1>
+      <br />
+      <img src={image}></img>
+      <p> {name} </p>
+      <p> {rating} stars</p>
+      <Link to="/playlist">Select</Link>
+      <Route path="/playlist" exact>
+        <PlaylistPage />
+      </Route>
     </div>
   );
 };
--- client/src/components/Playlists/Playlists.jsx
+++ client/src/components/Playlists/Playlists.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Playlists.scss';
+import Playlist from "./Playlist/Playlist"
 
-export default function Playlists() {
+export default function Playlists({playlists}) {
+  const playlist = playlists.map((playlist) => {
+    return <Playlist 
+              key={playlist.playlistId}
+              name={playlist.playlistName}
+              image={playlist.playlistPhoto}
+              rating={playlist.playlistRating}>
+          </Playlist>
+  })
   return (
     <div className="playlists">
-      <h1>I am Playlists</h1>
+      <h1>Playlists Page</h1>
+      <li>{playlist}</li>
     </div>
   );
 };
