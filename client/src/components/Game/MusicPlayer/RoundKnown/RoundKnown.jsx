cdb550ed77beeee87b9856c491643516b881379b
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-09 20:43:48 -0400

    update round known

--- client/src/components/Game/MusicPlayer/RoundKnown/RoundKnown.jsx
+++ client/src/components/Game/MusicPlayer/RoundKnown/RoundKnown.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import './RoundKnown.scss';
 
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  cover: {
-    height: 400,
-    width: 400,
-    borderRadius: "12px"
-  }
-}));
-
 export default function RoundKnown({song}) {
-  const classes = useStyles();
 
   return (
     <div className="round-known">
-      {/* For now, assume someone will always guess song */}
-      <h6>SONG - KNOW When a song is guessed or no one guesses (display album and details)</h6>
       <img src={song.albumPhoto} alt="cover" />
-
     </div>
   );
 };
