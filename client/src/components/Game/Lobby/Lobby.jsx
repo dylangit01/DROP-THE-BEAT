ffe447fc0ca1f4917120498c0a05f7e12213ba5e
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-10 19:12:12 -0400

    Adjsut styling for button in Lobby

--- client/src/components/Game/Lobby/Lobby.jsx
+++ client/src/components/Game/Lobby/Lobby.jsx
@@ -18,13 +18,13 @@ import { withStyles } from '@material-ui/core/styles';
 
 const StyledLobbyBtnOne = withStyles({
   root: {
-    background: 'linear-gradient(45deg, #867ae9 30%, #f25287 90%)',
+    background: 'linear-gradient(45deg, #867ae9 30%, #2162f3 90%)',
     borderRadius: 5,
     border: 0,
     color: 'white',
     height: 50,
     padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     marginLeft: 180,
   },
   label: {
@@ -34,13 +34,13 @@ const StyledLobbyBtnOne = withStyles({
 
 const StyledCodeBtnTwo = withStyles({
   root: {
-    background: 'linear-gradient(45deg, #867ae9 30%, #f25287 90%)',
+    background: 'linear-gradient(45deg, #867ae9 30%, #2162f3 90%)',
     borderRadius: 5,
     border: 0,
     color: 'white',
     height: 58,
     padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     marginLeft: 70,
   },
   label: {
