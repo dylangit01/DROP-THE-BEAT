ec1431edcf0652ddf5fe0bc39a736863ad96c7ad
Dylan <dcren2010@gmail.com>
2021-06-10 13:03:09 -0400

    remove unused elements

--- client/src/components/Game/Lobby/Lobby.jsx
+++ client/src/components/Game/Lobby/Lobby.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { SET_PLAYLIST } from '../../../reducer/data_reducer';
+import React from 'react';
+// import { SET_PLAYLIST } from '../../../reducer/data_reducer';
 import Ruby from '../../../assets/Ruby_logo.png'
 
 // Styling
@@ -15,22 +15,6 @@ import {
 } from '@material-ui/core';
 import useStyles from './LobbyStyles';
 import { withStyles } from '@material-ui/core/styles';
-
-const StyledLobbyBtnOne = withStyles({
-  root: {
-    background: 'linear-gradient(45deg, #867ae9 30%, #f25287 90%)',
-    borderRadius: 5,
-    border: 0,
-    color: 'white',
-    height: 50,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    marginLeft: 180,
-  },
-  label: {
-    textTransform: 'capitalize',
-  },
-})(Button);
 
 const StyledCodeBtnTwo = withStyles({
   root: {
