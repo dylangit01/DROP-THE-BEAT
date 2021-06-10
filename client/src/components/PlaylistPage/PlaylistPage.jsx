5a40d18ef3bc9c9cd7dcc18bf3c5e7ee039f4aae
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-10 17:38:48 -0400

    Make buttons consistent colour

--- client/src/components/PlaylistPage/PlaylistPage.jsx
+++ client/src/components/PlaylistPage/PlaylistPage.jsx
@@ -12,13 +12,13 @@ import { withStyles } from '@material-ui/core/styles';
 
 const StyledLobbyBtnOne = withStyles({
   root: {
-    background: 'linear-gradient(45deg, #867ae9 30%, #f25287 90%)',
+    background: 'linear-gradient(45deg, #867ae9 30%, #2162f3 90%)',
     borderRadius: 5,
     border: 0,
     color: 'white',
     height: 50,
     padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     marginLeft: 180,
   },
   label: {
@@ -28,13 +28,13 @@ const StyledLobbyBtnOne = withStyles({
 
 const StyledCodeBtnTwo = withStyles({
   root: {
-    background: 'linear-gradient(45deg, #867ae9 30%, #f25287 90%)',
+    background: 'linear-gradient(45deg, #867ae9 30%, #2162f3 90%)',
     borderRadius: 5,
     border: 0,
     color: 'white',
     height: 58,
     padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     marginLeft: 70,
   },
   label: {
