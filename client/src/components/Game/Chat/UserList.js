c3e2b1cd09431183a64720c9d5ae12cfe3079e9b
Dylan <dcren2010@gmail.com>
2021-06-10 23:17:02 -0400

    create chat-socket

--- client/src/components/Game/Chat/UserList.js
+++ client/src/components/Game/Chat/UserList.js
@@ -3,9 +3,10 @@ import './Chat.scss'
 
 const UserList = (props) => {
   return (
-    <ul style={{padding: '5px'}}>
-      {props.users.map((user) => (
-        <div className='user-list'>
+    <ul style={{ padding: '5px', marginTop: '5px' }}>
+      <p>Players:</p>
+      {props.users.map((user, idx) => (
+        <div key={idx} className='user-list'>
           <img style={{ width: '2vw', height:'3vh', marginRight:'5px' }} src={Ruby} alt='' />
           <li style={{ listStyleType: 'none' }} key={user.id}>
             {user.name}
