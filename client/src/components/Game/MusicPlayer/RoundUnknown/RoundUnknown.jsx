8ee72a08202a86d6ade679b002ef8089c9ad2746
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-10 21:05:16 -0400

    Add ? to main album cover

--- client/src/components/Game/MusicPlayer/RoundUnknown/RoundUnknown.jsx
+++ client/src/components/Game/MusicPlayer/RoundUnknown/RoundUnknown.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './RoundUnknown.scss';
+import HelpOutlineOutlinedIcon from '@material-ui/icons/HelpOutlineOutlined';
 
 
 export default function RoundUnknown() {
   return (
     <div className="round-unknown">
       <div>
-        <h6>SONG UNKNOWN</h6>
+        <HelpOutlineOutlinedIcon className="question-mark"/>
       </div>
     </div>
   );
