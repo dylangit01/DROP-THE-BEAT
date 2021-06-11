4be61580c5e9871fc4febab9bec0141c23c332f9
Dylan <dcren2010@gmail.com>
2021-06-11 06:09:48 -0400

    add input styles

--- client/src/components/Game/Chat/MessageInput/index.js
+++ client/src/components/Game/Chat/MessageInput/index.js
@@ -1,17 +1,31 @@
-
+import { makeStyles } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import './index.scss'
 const MessageInput = (props) => {
-	
   const onSubmit = (evt) => {
     evt.preventDefault();
     props.onSubmit('SEND_MESSAGE',evt.target.message.value);
     evt.target.message.value = '';
   };
 
+  const useStyles = makeStyles((theme) => ({
+    button: {
+      background: 'linear-gradient(45deg, #9e66f2 30%, #2162f3 90%)',
+      color: 'white',
+      width: 100,
+      height:40,
+      position: 'absolute',
+      right:0,
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <form onSubmit={onSubmit} className='messageInput'>
       <div className='chat-input-box'>
         <input className='chat-input' type='text' name='message' placeholder='Type your message' />
-        <button>Send!</button>
+        <Button className={classes.button} type='submit'>Send!</Button>
       </div>
     </form>
   );
