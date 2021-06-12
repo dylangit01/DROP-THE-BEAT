1116e2d95ada4bf7822ca020270d409854121fd7
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-12 18:49:53 -0400

    Add comment about old message list code

--- client/src/components/Game/Chat/MessageList.js
+++ client/src/components/Game/Chat/MessageList.js
@@ -1,4 +1,4 @@
-import './Chat.scss'
+import './Chat.scss';
 
 const MessageList = ({ messages, users }) => {
   console.log('messages: ', messages);
@@ -10,21 +10,21 @@ const MessageList = ({ messages, users }) => {
     //     console.log(xxx);
     //     return xxx
     //   }
-    //   return 
+    //   return
     // }
     const user = users.find((user) => user.id === msg.id);
     if (user) {
-      return user.name
+      return user.name;
     }
-    
-  }
+  };
 
   return (
-    <ul className='message-list'>
+    <ul className="message-list">
       {messages.map((msg, id) => (
-        <li key={id} className='message-list--item'>
+        <li key={id} className="message-list--item">
           <b style={{ color: msg.color }}>{getUsernameForMsg(users, msg)}: </b>
           {/* <b style={{ color: msg.color }}>{msg.name}: </b> */}
+          {/* This old way should work and should be good because you don't need the extra step since each msg has the attached name to it :) */}
           {msg.msg} ------- score {msg.score}
         </li>
       ))}
