3d77cac31d708b26f898273691043cc08aa5efd0
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-11 22:08:34 -0400

    Add emojis to result page

--- client/src/components/Game/Result/Result.jsx
+++ client/src/components/Game/Result/Result.jsx
@@ -7,7 +7,7 @@ export default function Result({ score, winner }) {
 
   return (
     <div className="result">
-      <Typography variant='h4'>Congrats {winner}!</Typography>
+      <Typography variant='h4'>Congrats 🎉 {winner} 🎉</Typography>
       <Typography variant='h6'>You are the beatmaker of the game!</Typography>
       <img className="trophy" src="https://github.com/dylangit01/DROP-THE-BEAT/blob/ruby/feature/winner-page/client/public/images/trophy.png?raw=true" alt="trophy" />
     </div>
