787b72e1b6b6652d439d793a03ad013112c793bd
Dylan <dcren2010@gmail.com>
2021-06-13 10:34:47 -0400

    add ani to correct guess

--- client/src/components/Game/Chat/MessageList.js
+++ client/src/components/Game/Chat/MessageList.js
@@ -5,9 +5,23 @@ import { makeStyles } from '@material-ui/styles';
 
 const MessageList = ({ messages, users }) => {
   const useStyles = makeStyles((theme) => ({
+    '@keyframes bingo': {
+      '0%': {},
+      '50%': {},
+      '100%': {
+        transform: 'scale(1.2, 1.2)',
+      },
+      '30%': {},
+      '80%': {
+        transform: 'scale(1, 1)',
+      },
+    },
     musicIcon: {
       height: '90%',
       // color: "linear-gradient(45deg, #9e66f2 30%, #2162f3 90%)"
+    },
+    bingo: {
+      animation: '$bingo 1s ease 0s infinite',
     },
   }));
 
@@ -26,10 +40,9 @@ const MessageList = ({ messages, users }) => {
         <li key={idx} className='message-list--item'>
           <MusicNoteRoundedIcon style={{ color: msg.color }} className={classes.musicIcon}></MusicNoteRoundedIcon>
           <b style={{ color: msg.color }}>{getUsernameForMsg(users, msg)}: &nbsp; </b>
-          {
-            
-          }
-          <b style={{ color: msg.msgColor }}>  {msg.msg}</b> ------- score {msg.score}
+          <b style={{ color: msg.msgColor }} className={ msg.msgColor === '#54e346' &&  classes.bingo}>
+            {msg.msg}
+          </b>------- score {msg.score}
         </li>
       ))}
     </ul>
--- client/src/components/Game/Score/Score.jsx
+++ client/src/components/Game/Score/Score.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Score.scss';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
 
 export default function Score({ setScore, setWinner, user, users, messages }) {
 
   return (
     <div className='score-box'>
       {users.map((user, idx) => (
-        <div key={idx}>
+        <div key={idx} className='scoreDetail'>
+          <MusicNoteIcon></MusicNoteIcon>
           <p>{user.name}</p>
           <p>{user.score}</p>
+          <MusicNoteIcon></MusicNoteIcon>
         </div>
       ))}
     </div>
