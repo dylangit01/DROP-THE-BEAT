10394bbef2956247c4de2d2fd646cbacae8dfc05
Dylan <dcren2010@gmail.com>
2021-06-14 04:50:42 -0400

    change score font style

--- client/src/components/Game/Score/Score.jsx
+++ client/src/components/Game/Score/Score.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import './Score.scss';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import { makeStyles } from '@material-ui/styles';
 
 export default function Score({ setScore, setWinner, user, players, messages }) {
+
   const useStyles = makeStyles((theme) => ({
     player: {
       fontSize: '15px',
@@ -15,7 +15,6 @@ export default function Score({ setScore, setWinner, user, players, messages }) 
   }));
 
   const classes = useStyles();
-  console.log(players);
 
   return (
     <div className='score-box'>
@@ -24,29 +23,29 @@ export default function Score({ setScore, setWinner, user, players, messages }) 
       {/* 1st player */}
       {players[0] && (
         <div className='scoreDetail'>
-        <div className='iconNum'>
-          <MusicNoteIcon className={classes.mIcon} style={{ color: players[0].color, width: '30px' }}></MusicNoteIcon>
-          <p className='scoreNum' style={{ color: '#54e346' }}>
-            {players[0].score}
-          </p>
+          <div className='iconNum'>
+            <MusicNoteIcon className={classes.mIcon} style={{ color: players[0].color, width: '30px' }}></MusicNoteIcon>
+            <p className='scoreNum' style={{ color: '#54e346' }}>
+              {players[0].score}
+            </p>
+          </div>
+          <p className={classes.player}>{players[0].name}</p>
         </div>
-        <p className={classes.player}>{players[0].name}</p>
-      </div>
       )}
 
       {/* 2nd player */}
       {players[1] && (
         <div className='scoreDetail'>
-        <div className='iconNum'>
-          <p className='scoreNum' style={{ color: '#54e346' }}>
-            {players[1].score}
-          </p>
-          <MusicNoteIcon className={classes.mIcon} style={{ color: players[1].color, width: '30px' }}></MusicNoteIcon>
+          <div className='iconNum'>
+            <p className='scoreNum' style={{ color: '#54e346' }}>
+              {players[1].score}
+            </p>
+            <MusicNoteIcon className={classes.mIcon} style={{ color: players[1].color, width: '30px' }}></MusicNoteIcon>
+          </div>
+          <p className={classes.player}>{players[1].name}</p>
         </div>
-        <p className={classes.player}>{players[1].name}</p>
-      </div>
       )}
-      
+
       {/* ))} */}
     </div>
   );
