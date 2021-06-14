0efc76903508294d0dcfe82d0d567e21334ceb73
Nelli Butenko <butenkonelly93@gmail.com>
2021-06-14 01:02:47 +0000

    Set up the countdown state

--- client/src/components/Game/GameInProgress/GameInProgress.jsx
+++ client/src/components/Game/GameInProgress/GameInProgress.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Chat from "../Chat/Chat";
 import Score from "../Score/Score";
@@ -7,34 +7,58 @@ import TrackList from "../TrackList/TrackList";
 import './GameInProgress.scss';
 import MessageInput from '../Chat/MessageInput/index'
 
-  
+
 export default function GameInProgress({ playlist, nextRound, song, round, setRound,user, users, messages, sendMessage, host, players}) {
 
+  const [counter, setCounter] = useState(3);
+  
+  useEffect(() => {
+    const timer =
+      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    return () => clearInterval(timer);
+  }, [counter]);
+
   return (
+
     <div className='game-in-progress'>
+
       <div className='left-side'>
-        {song && (
-          <MusicPlayer
-            round={round}
-            setRound={setRound}
-            playlist={playlist}
-            song={song}
-            nextRound={() => nextRound()}
-            sendMessage={sendMessage}
-            user={user}
-            host={host}
-            players={players}
-          />
+
+        {counter !== 0 && (
+          <div className="countdown">
+            <span>{counter}</span>
+          </div>
         )}
-        <TrackList round={round} songs={playlist.songs} />
 
+        {counter === 0 && (
+          <>
+            {song && (
+              <div className="music-player">
+                <MusicPlayer
+                  round={round}
+                  setRound={setRound}
+                  playlist={playlist}
+                  song={song}
+                  nextRound={() => nextRound()}
+                  sendMessage={sendMessage}
+                  user={user}
+                  host={host}
+                  players={players}
+                />
+              </div>
+            )}
+            <TrackList round={round} songs={playlist.songs} />
+          </>
+        )}
 
       </div>
+
       <div className='right-side'>
-        <Score {...{ user, players, messages }} />
-        <Chat {...{ user, users, messages }} />
-        <MessageInput onSubmit={sendMessage} />
-      </div>
+      <Score {...{ user, players, messages }} />
+      <Chat {...{ user, users, messages }} />
+      <MessageInput onSubmit={sendMessage} />
+    </div>
+
     </div>
   );
 }
