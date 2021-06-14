adf380ce0bb3062b0cd965757a654b72d00c231f
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-13 22:34:23 -0400

    Update basic styles and add one list item

--- client/src/components/PlaylistPage/PlaylistPage.jsx
+++ client/src/components/PlaylistPage/PlaylistPage.jsx
@@ -5,6 +5,24 @@ import { DTBContext } from '../../contextAPI/DTBContext';
 // Styling
 import './PlaylistPage.scss';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import TimelapseIcon from '@material-ui/icons/Timelapse';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    color: 'white',
+    background: 'transparent',
+  },
+  icon: {
+    backgroundColor: 'pink',
+  }
+}));
 
 export default function PlaylistPage({ dispatch, gameLink }) {
   // const classes = useStyles();
@@ -20,6 +38,8 @@ export default function PlaylistPage({ dispatch, gameLink }) {
     setPlaylist(playlists[idNum]);
   }, [playlists, idNum, setPlaylist]);
 
+  const classes = useStyles();
+
   const handlePlaylistClick = (event) => {
     history.push('/game/12345')
   };
@@ -29,12 +49,24 @@ export default function PlaylistPage({ dispatch, gameLink }) {
       {playlist && (
         <div className='playlist-page'>
           <div className='playlist-page-left'>
-            <Typography variant='h4'>{playlist.playlistName} Playlist</Typography>
+            <Typography variant='h4' gutterBottom>{playlist.playlistName} Playlist</Typography>
             <img src={playlist.playlistPhoto} alt="playlistPhoto"></img>
           </div>
           <div className='playlist-page-right'>
             <Typography variant='h4'>Right Side</Typography>
-            
+            <List className={classes.root}>
+              <ListItem >
+                <ListItemAvatar >
+                  <Avatar className={classes.icon}>
+                    <TimelapseIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText>
+                <Typography variant='h6'>Difficulty</Typography>
+
+                </ListItemText>
+              </ListItem>
+            </List>
           </div>
         </div>
       )}
