d57372a10c8dd2175e1ff81af2c43441a190e9ce
Dylan <dcren2010@gmail.com>
2021-06-15 15:38:05 -0400

    add audioVis component

---
+++ client/src/components/Game/MusicPlayer/Audio/AudioVisualizer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const AudioVisualizer = ({ song }) => {
+	// const preload = () => {
+	// }
+
+	// const setup = () => {
+	// 	createCanvas(windowWidth, windowHeight);
+	// }
+
+	// const draw = () => {
+	// 	background(0)
+	// }
+
+
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default AudioVisualizer
