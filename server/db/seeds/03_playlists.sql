c3cb27e6fd031517ee717f426bd5fe7e51cbcad1
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-06-16 10:07:41 -0400

    Change rating of 2am coding sessions to be higher

--- server/db/seeds/03_playlists.sql
+++ server/db/seeds/03_playlists.sql
@@ -4,7 +4,7 @@ VALUES
   ('Christmas Mood', 'https://github.com/dylangit01/DROP-THE-BEAT/blob/Dylan/feature-16-Rating/client/public/images/playlist/christmas-1.jpg?raw=true', 4.3, 1),
   ('K-Poppin''', 'https://github.com/dylangit01/DROP-THE-BEAT/blob/main/client/public/images/playlist/kpop.jpg?raw=true', 3.4, 1),
   ('Pandemic', 'https://github.com/dylangit01/DROP-THE-BEAT/blob/Dylan/feature-16-Rating/client/public/images/playlist/pandemic-1.jpg?raw=true', 4.3, 1),
-  ('2AM Coding Sessions', 'https://github.com/dylangit01/DROP-THE-BEAT/blob/Dylan/feature-16-Rating/client/public/images/playlist/coding-1.jpg?raw=true', 2.6, 1),
+  ('2AM Coding Sessions', 'https://github.com/dylangit01/DROP-THE-BEAT/blob/Dylan/feature-16-Rating/client/public/images/playlist/coding-1.jpg?raw=true', 4.9, 1),
   ('Coffee Shop Vibes', 'https://github.com/dylangit01/DROP-THE-BEAT/blob/Dylan/feature-16-Rating/client/public/images/playlist/coffee-1.jpg?raw=true', 3.7, 1),
   ('Beyoncé ♥', 'https://github.com/dylangit01/DROP-THE-BEAT/blob/main/client/public/images/playlist/beyonce.jpeg?raw=true', 3.2, 1),
   ('G.O.A.T Love Songs', 'https://github.com/dylangit01/DROP-THE-BEAT/blob/Dylan/feature-16-Rating/client/public/images/playlist/love-1.jpg?raw=true', 4.4, 1),
